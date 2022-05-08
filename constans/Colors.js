9648c22d5a6a80b1defb80da66b1a1aa7d060f04
1hakann <hakan.cakmak@live.com>
2022-05-09 00:37:28 +0300

    App layout has been completed.

---
+++ constans/Colors.js
@@ -0,0 +1,14 @@
+export default {
+    white: '#fff',
+    black: '#1c2b2d',
+    blue: '#31326f',
+    darkBlue: '#005490',
+    purple: '#5d20be',
+    pink: '#ca9fd6',
+    warning: '#ffcf21',
+    orange :'#db6400',
+    red: '#a84667',
+    grey: '#eee',
+    darkGreen: '#222831',
+    green: '#8eff65',
+}
